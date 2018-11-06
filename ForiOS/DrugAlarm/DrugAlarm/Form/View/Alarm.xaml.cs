88b15e2a0375d9132911fe61da11b4dc7bda5be4
山本陽明 <akiyam0613@gmail.com>
2018-11-06 09:31:24 +0900

    Xamarin:画面開発:アラーム

---
+++ ForiOS/DrugAlarm/DrugAlarm/Form/View/Alarm.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DrugAlarm.Form.View
+{
+    public partial class Alarm : ContentPage
+    {
+        public Alarm()
+        {
+            InitializeComponent();
+        }
+    }
+}
