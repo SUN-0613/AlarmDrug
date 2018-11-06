b3af08c1ff47b5a78f4e4d6f349b4b2de9ab2acd
山本陽明 <akiyam0613@gmail.com>
2018-11-06 09:20:17 +0900

    Xamarin:画面開発:お知らせ

---
+++ ForiOS/DrugAlarm/DrugAlarm/Form/View/Information.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DrugAlarm.Form.View
+{
+    public partial class Information : ContentPage
+    {
+        public Information()
+        {
+            InitializeComponent();
+        }
+    }
+}
