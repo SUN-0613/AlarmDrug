a0c4c49ab280b53f4c743f11d9eec01260cdd2ad
山本陽明 <akiyam0613@gmail.com>
2019-01-10 13:13:34 +0900

    Xamarin:iOS:ListViewのハイライト色変更

---
+++ ForiOS/DrugAlarm/DrugAlarm.iOS/ViewCellCustomRenderer.cs
@@ -0,0 +1,40 @@
+﻿using DrugAlarm.Common;
+using DrugAlarm.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(ViewCellCustom), typeof(ViewCellCustomRenderer))]
+
+namespace DrugAlarm.iOS.Renderers
+{
+
+    /// <summary>
+    /// View Cell Custom Renderer
+    /// </summary>
+    public class ViewCellCustomRenderer : ViewCellRenderer
+    {
+
+        /// <summary>
+        /// セル取得
+        /// </summary>
+        /// <returns>The cell.</returns>
+        /// <param name="item">Item.</param>
+        /// <param name="reusableCell">Reusable cell.</param>
+        /// <param name="tv">Tv.</param>
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+
+            var cell = base.GetCell(item, reusableCell, tv);
+
+            cell.SelectedBackgroundView = new UIView
+            {
+                BackgroundColor = ((ViewCellCustom)item).SelectedBackgroundColor.ToUIColor()
+            };
+
+            return cell;
+
+        }
+
+    }
+}
---
+++ ForiOS/DrugAlarm/DrugAlarm/Common/ViewCellCustom.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+
+namespace DrugAlarm.Common
+{
+
+    /// <summary>
+    /// View cell custom.
+    /// </summary>
+    public class ViewCellCustom : ViewCell
+    {
+
+        /// <summary>
+        /// 選択ハイライト背景色プロパティ
+        /// </summary>
+        public static readonly BindableProperty SelectedBackgroundColorProperty 
+            = BindableProperty.Create(nameof(SelectedBackgroundColor), typeof(Color), typeof(ViewCellCustom), Color.Default);
+
+        /// <summary>
+        /// 選択ハイライト背景色
+        /// </summary>
+        /// <value>The color of the selected background.</value>
+        public Color SelectedBackgroundColor
+        {
+            get { return (Color)GetValue(SelectedBackgroundColorProperty); }
+            set { SetValue(SelectedBackgroundColorProperty, value); }
+        }
+
+        /// <summary>
+        /// new
+        /// </summary>
+        public ViewCellCustom()
+        {
+        }
+
+    }
+
+}
